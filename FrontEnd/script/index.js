5a74670bb4c342a0dddb567a5ed86f7fa7ed9df5
Kunal Pal <kunal17265@gmail.com>
2023-02-22 23:38:56 +0530

    integrated Stripe Payment Gateway

--- FrontEnd/script/index.js
+++ FrontEnd/script/index.js
@@ -4,11 +4,53 @@ button.addEventListener("click", function (event) {
   checkoutPay();
 });
 
-function checkoutPay() {
-  Swal.fire({
-    icon: "error",
-    title: "Oops...",
-    text: "All fields are required",
-    // footer: '<a href="">Why do I have this issue?</a>',
-  });
+async function checkoutPay() {
+  let input_fields = {
+    name: document.getElementById("name-input-field").value,
+    email: document.getElementById("email-input-field").value,
+    amount: document.getElementById("amount-input-field").value,
+  };
+
+  if (!input_fields.name || !input_fields.email || !input_fields.amount) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "All fields are required",
+      // footer: '<a href="">Why do I have this issue?</a>',
+    });
+  } else {
+    console.log(
+      "it's working" +
+        input_fields.name +
+        " " +
+        input_fields.email +
+        " " +
+        input_fields.amount
+    );
+
+    await fetch("/create-checkout-session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: input_fields.name,
+        email: input_fields.email,
+        amount: input_fields.amount,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        //if error, then send reject to catch
+        return res.json().then((json) => Promise.reject(json));
+      })
+      //Stripe sends a url after connection
+      .then(({ url }) => {
+        window.location = url;
+        //console.log(url);
+      })
+      .catch((e) => {
+        console.log(e.error);
+      });
+  }
 }
--- app.js
+++ app.js
@@ -1,14 +1,63 @@
 let express = require("express");
 let app = express();
+app.use(express.static(__dirname + "/FrontEnd"));
 
 require("dotenv").config();
+var bodyParser = require("body-parser");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
+// const storeItems = new Map();
 
 app.listen(process.env.PORT, () => {
   console.log("Listening on port " + process.env.PORT);
 });
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/FrontEnd/index.html");
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    //takes input for stripe and gives a session as ouput
+    const session = await stripe.checkout.sessions.create({
+      //payment_method_types: ["card", "google_pay", "upi", "paytm"],
+      payment_method_types: ["card"],
+      //payment mode for one time transaction, or other for subscription
+      mode: "payment",
+      //billing_address_collection: "required",
+      line_items: [
+        {
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: `Name: ${req.body.name}`, // Replace with your product name
+              description: `Email: ${req.body.email}`, // Replace with your product description
+            },
+            unit_amount: 100 * req.body.amount,
+          },
+          quantity: 1, // you may want to adjust the quantity as needed
+        },
+      ],
+      success_url: `${process.env.SERVER_URL}/success`,
+      cancel_url: `${process.env.SERVER_URL}/cancel`,
+    });
+    res.json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 
-app.use(express.static(__dirname + "/FrontEnd"));
+//Only app.use is required
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + "/FrontEnd/index.html");
+// });
+
+app.get("/success", function (req, res) {
+  res.sendFile(__dirname + "/FrontEnd/success.html");
+});
+
+app.get("/cancel", function (req, res) {
+  res.sendFile(__dirname + "/FrontEnd/failed.html");
+});
+
+//for card number, put "4242424242"
